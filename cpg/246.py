1480da6717fd46e5bf52af033f4211264813db1a
Ozturk Gokal <ogokal@gmail.com>
2020-11-03 00:35:01 +0300

    246

---
+++ cpg/246.py
@@ -0,0 +1,10 @@
+
+# https://www.lintcode.com/problem/strobogrammatic-number/description
+class Solution:
+    """
+    @param num: a string
+    @return: true if a number is strobogrammatic or false
+    """
+
+    def isStrobogrammatic(self, num):
+        # write your code here
--- cpg/299.py
+++ cpg/299.py
@@ -1,2 +1,23 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
+        bulls = {}
+        lg = len(guess)
+        ls = len(secret)
+        pc = {}
+        for i in range(ls):
+            if i < lg:
+                if secret[i] == guess[i]:
+                    bulls[i] = secret[i]
+                else:
+                    if secret[i] not in pc:
+                        pc[secret[i]] = 0
+                    pc[secret[i]] += 1
+        cows = 0
+        for i, g in enumerate(guess):
+            if i not in bulls and g in pc and pc[g] > 0:
+                pc[g] -= 1
+                cows += 1
+        return str(len(bulls))+"A"+str(cows)+"B"
+
+
+print(Solution().getHint("11", "01"))
